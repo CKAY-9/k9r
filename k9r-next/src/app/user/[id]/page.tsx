3ddd1cc594e209312a6031612488ea4dfa9a5406
Cameron A. <foxtrotcyanide@gmail.com>
2025-04-05 11:22:49 -0700

    Fix background issues with size and parallax.

--- k9r-next/src/app/user/[id]/page.tsx
+++ k9r-next/src/app/user/[id]/page.tsx
@@ -8,55 +8,69 @@ import { getStoredCookie } from "@/utils/stored-cookies";
 import { Metadata } from "next";
 import UserProfileClient from "./client";
 
-export const generateMetadata = async ({ params }: {
-    params: Promise<{
-        id: string
-    }>
+export const generateMetadata = async ({
+	params,
+}: {
+	params: Promise<{
+		id: string;
+	}>;
 }): Promise<Metadata> => {
-    const details = await getCommunityDetails();
+	const details = await getCommunityDetails();
 
-    const { id } = await params;
-    const user = await getUserFromID(Number.parseInt(id));
+	const { id } = await params;
+	const user = await getUserFromID(Number.parseInt(id));
 
-    if (user === null) {
-        return {
-            title: `Invalid User - ${details.name}`,
-            description: `Failed to find user with specified ID on ${details.name}. ${details.description}`
-        }
-    }
+	if (user === null) {
+		return {
+			title: `Invalid User - ${details.name}`,
+			description: `Failed to find user with specified ID on ${details.name}. ${details.description}`,
+		};
+	}
 
-    return {
-        title: `${user.display_name}'s Profile - ${details.name}`,
-        description: `${user.display_name}'s profile on ${details.name}. ${details.description}`,
-    };
-}
+	return {
+		title: `${user.display_name}'s Profile - ${details.name}`,
+		description: `${user.display_name}'s profile on ${details.name}. ${details.description}`,
+	};
+};
 
-const UserPage = async ({ params }: {
-    params: Promise<{
-        id: string
-    }>
+const UserPage = async ({
+	params,
+}: {
+	params: Promise<{
+		id: string;
+	}>;
 }) => {
-    const details = await getCommunityDetails();
+	const details = await getCommunityDetails();
 
-    const user_token = await getStoredCookie("token");
-    const personal_user = await getPersonalUser(user_token);
+	const user_token = await getStoredCookie("token");
+	const personal_user = await getPersonalUser(user_token);
 
-    const { id } = await params;
-    const user = await getUserFromID(Number.parseInt(id));
-    
-    return (
-        <>
-            <Header community_details={details} personal_user={personal_user} />
-            <main className="container" style={{"background": `url(${user?.banner})`}}>
-                {user === null ? (
-                    <h1>Failed to find user with specified ID</h1>
-                ) : (
-                    <UserProfileClient user={user} community_details={details} />
-                )}
-            </main>
-            <Footer community_details={details} />
-        </>
-    );
-}
+	const { id } = await params;
+	const user = await getUserFromID(Number.parseInt(id));
+
+	return (
+		<>
+			<Header community_details={details} personal_user={personal_user} />
+			<main
+				className="container"
+				style={{
+					background: `url(${user?.banner})`,
+					backgroundSize: "100% 100%",
+                    backgroundAttachment: "fixed"
+				}}
+			>
+				{user === null ? (
+					<h1>Failed to find user with specified ID</h1>
+				) : (
+					<UserProfileClient
+						user={user}
+						community_details={details}
+					/>
+				)}
+			</main>
+			<Footer community_details={details} />
+		</>
+	);
+};
 
 export default UserPage;
--- k9r-next/src/components/user/container/user-container.tsx
+++ k9r-next/src/components/user/container/user-container.tsx
@@ -4,18 +4,26 @@ import { User } from "@/api/users/models";
 import style from "./container.module.scss";
 
 type UserContainerProps = {
-    user: User;
-    children: any;
+	user: User;
+	children: any;
 };
 
 const UserContainer = (props: UserContainerProps) => {
-    return (
-        <>
-            <div className={style.user_container} style={{"backgroundColor": props.user.banner.length >= 1 ? "rgb(0, 0, 0, 0.75)" : "initial"}}>
-                {props.children}
-            </div>
-        </>
-    );
-}
+	return (
+		<>
+			<div
+				className={style.user_container}
+				style={{
+					backgroundColor:
+						props.user.banner.length >= 1
+							? "rgb(0, 0, 0, 0.75)"
+							: "initial",
+				}}
+			>
+				{props.children}
+			</div>
+		</>
+	);
+};
 
 export default UserContainer;
